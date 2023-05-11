817ba7ca110beacefdc06db301a19d439580350e
Marefo <79011582+Marefo@users.noreply.github.com>
2023-05-11 13:46:21 +0300

    created visual style

---
+++ Assets/_CodeBase/Extensions/Extensions.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SystemRandom = System.Random;
+
+namespace _CodeBase.Extensions
+{
+  public static class Extensions
+  {
+    private static readonly SystemRandom rng = new SystemRandom(); 
+		
+    public static T GetRandomValue<T>(this List<T> list) => list[Random.Range(0, list.Count)];
+		
+    public static List<T> Shuffle<T>(this List<T> list)
+    {
+      List<T> result = new List<T>(list);
+      int n = result.Count;  
+      while (n > 1) {  
+        n--;  
+        int k = rng.Next(n + 1);
+        (result[k], result[n]) = (result[n], result[k]);
+      }
+      return result;
+    }
+		
+    public static bool AddIfNotExists<T>(this List<T> list, T value)
+    {
+      if (list.Contains(value)) return false;
+			
+      list.Add(value);
+      return true;
+    }
+    
+    public static bool CompareLayers(this GameObject obj, LayerMask layerMask) => 
+      layerMask == (layerMask | (1 << obj.layer));
+
+    public static void ChangeImageAlpha(this Image image, float targetAlpha)
+    {
+      Color imageColor = image.color;
+      imageColor.a = targetAlpha;
+      image.color = imageColor;
+    }
+    
+    public static int GetSignForInterpolation(this float currentValue, float targetValue) => 
+      targetValue > currentValue ? 1 : -1;
+  }
+}
